9f6d73bec5a4ecfa4e39cf5c5dfa9d4967953b7c
Chris <cgm19@pitt.edu>
2016-03-12 23:48:22 +0000

    Can read twilio keys successfully

--- Hermes.py
+++ Hermes.py
@@ -7,6 +7,14 @@ def isValid(text):
     return bool(re.search(r'\bsend text message\b', text, re.IGNORECASE))
 
 def handle(text, mic, profile):
+    
+    account_sid = profile["TWILIO_ACCOUNT_SID"]
+    auth_token = profile["TWILIO_AUTH_TOKEN"]
+
+    print account_sid
+    print auth_token
+    
+
     message = "Sending text message"
     mic.say(message)
 
