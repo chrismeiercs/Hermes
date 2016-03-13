f25237280f8364611f3730f921f6f02ab9e5dfa4
Chris <cgm19@pitt.edu>
2016-03-13 00:04:38 +0000

    Can read in contacts' phone numbers

--- Hermes.py
+++ Hermes.py
@@ -11,12 +11,22 @@ def handle(text, mic, profile):
     account_sid = profile["TWILIO_ACCOUNT_SID"]
     auth_token = profile["TWILIO_AUTH_TOKEN"]
 
-    print account_sid
-    print auth_token
-    
+    mic.say("Who would you like to send it to?")
+
+    #contact = mic.activeListen()
+    contact = "MOM"
+    phone_number = getPhoneNumber(contact, profile)    
+
+    print phone_number
 
     message = "Sending text message"
     mic.say(message)
+
+def getPhoneNumber(contact, profile):
+    return profile["TWILIO_CONTACTS"][contact]
+
+def sendSMS():
+    return 1
 
 
 
